a737a28f1bf872042dc685bc916dd2dd4dfb11bc
diceguyd30 <michael@ennismightier.com>
2014-02-05 11:41:41 -0500

    Update eulertest.py

--- eulertest.py
+++ eulertest.py
@@ -1,5 +1,1 @@
-def main():
-    result = sum([x for x in range(1,1000) if x % 3 == 0 or x % 5 == 0])
-    print(result)
-    
-main()
+print(sum([x for x in range(1,1000) if x % 3 == 0 or x % 5 == 0]))
