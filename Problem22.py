7624b7da7280005968c0c36fc16754d4e063261f
Michael Ennis <michael@ennismightier.com>
2014-03-05 10:56:23 -0500

    Finished Problem 22

---
+++ Problem22.py
@@ -0,0 +1,8 @@
+lines = sorted(open("problem22.txt").read().replace('"', '').split(","))
+sums = map(lambda x: sum(map(lambda y: ord(y) - 64, x)), lines)
+
+result = 0
+for x in range(1, len(sums) + 1):
+    result += (x * sums[x-1])
+    
+print result
